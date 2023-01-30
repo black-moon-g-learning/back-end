e342e4c282300d438ad6ac46a6a1a67ca7afeaae
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-01-30 09:39:24 +0700

    add models

---
+++ app/Models/Continent.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Continent extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'name',
+        'description',
+        'image'
+    ];
+
+    /**
+     * @return HasMany
+     */
+    public function countries():HasMany{
+        return $this->hasMany(Country::class);
+    }
+
+}
---
+++ app/Models/Target.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Target extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string[]
+     */
+    protected $fillable = [
+        'time'
+    ];
+
+    /**
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
