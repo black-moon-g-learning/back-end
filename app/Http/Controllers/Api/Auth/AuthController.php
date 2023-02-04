ffcea34fbb3f707ccd20c7dbaeca1a81bbc680aa
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-02-04 08:51:09 +0700

    add register

--- app/Http/Controllers/Api/Auth/AuthController.php
+++ app/Http/Controllers/Api/Auth/AuthController.php
@@ -5,19 +5,30 @@ namespace App\Http\Controllers\Api\Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\AuthRequest;
 use App\Services\Auth\AuthService;
-use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
 
     protected $auth;
 
+    /**
+     * __construct
+     *
+     * @param  AuthService $auth
+     * @return void
+     */
     public function __construct(AuthService $auth)
     {
         $this->auth = $auth;
     }
 
 
+    /**
+     * login
+     *
+     * @param  AuthRequest $request
+     * @return void
+     */
     public function login(AuthRequest $request)
     {
         $response = $this->auth->login($request);
@@ -31,7 +42,15 @@ class AuthController extends Controller
         ], 422);
     }
 
+    /**
+     * register
+     *
+     * @param  AuthRequest $request
+     * @return void
+     */
     public function register(AuthRequest $request)
     {
+        $response = $this->auth->register($request);
+        return $this->responseSuccessWithData($response, 201);;
     }
 }
--- app/Services/Auth/AuthService.php
+++ app/Services/Auth/AuthService.php
@@ -2,11 +2,36 @@
 
 namespace App\Services\Auth;
 
+use App\Constants\Gender;
+use App\Constants\Role;
+use App\Constants\SocialMediaProvider;
+use App\Constants\User;
+use App\Repositories\User\IUserRepository;
+
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthService implements IAuthService
 {
+    protected $userRepo;
 
+    /**
+     * __construct
+     *
+     * @param  IUserRepository $userRepo
+     * @return void
+     */
+    public function __construct(IUserRepository $userRepo)
+    {
+        $this->userRepo = $userRepo;
+    }
+
+    /**
+     * login
+     *
+     * @param  mixed $request
+     * @return array
+     */
     public function login(mixed $request): array
     {
         $credentials = $request->validated();
@@ -31,10 +56,40 @@ class AuthService implements IAuthService
         ];
     }
 
+
+    /**
+     * register
+     *
+     * @param  mixed $request
+     * @return array
+     */
     public function register(mixed $request): array
     {
-        $userInfo = $request->validated();
-        
-        return [];
+
+        $userInfo = $this->setUpUserData($request->validated());
+
+        $this->userRepo->create($userInfo);
+
+        return  $userInfo;
+    }
+
+    /**
+     * inputUserDefaultData
+     *
+     * @param  array $userInfo
+     * @return array
+     */
+    public function setUpUserData(array $userInfo): array
+    {
+
+        $userInfo['role_id'] = Role::USER_ROLE;
+        $userInfo['provider_id'] = SocialMediaProvider::DEFAULT;
+        $userInfo['password'] = Hash::make($userInfo['password']);
+        $userInfo['image'] = User::DEFAULT_PROFILE_IMAGE;
+        $userInfo['gender'] = Gender::OTHER;
+
+        unset($userInfo['password_confirm']);
+
+        return $userInfo;
     }
 }
