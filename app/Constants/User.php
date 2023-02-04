756bae36bb7ed2fb9648f7284d39490b5170469d
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-02-04 08:47:38 +0700

    add constant for user

---
+++ app/Constants/User.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Constants;
+
+final class User
+{
+    const DEFAULT_PROFILE_IMAGE = "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Default_pfp.svg/1200px-Default_pfp.svg.png";
+}
