0074c7f21857519cfada4299e7f598e39cde9a7d
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-02-05 16:06:01 +0700

    format service

--- app/Services/Video/IVideoService.php
+++ app/Services/Video/IVideoService.php
@@ -7,4 +7,6 @@ interface IVideoService
     public function search(int $countryTopicId, string $search);
 
     public function index(int $countryTopicId);
+
+    public function formatResponse(string $type, mixed $data): array;
 }
--- app/Services/Video/VideoService.php
+++ app/Services/Video/VideoService.php
@@ -3,35 +3,75 @@
 namespace App\Services\Video;
 
 use App\Http\Resources\VideoResource;
+use App\Http\Resources\VideoSearchResource;
 use App\Repositories\Video\IVideoRepository;
 
 class VideoService implements IVideoService
 {
     protected IVideoRepository  $videoRepo;
 
+    /**
+     * __construct
+     *
+     * @param  IVideoRepository $videoRepo
+     * @return void
+     */
     public function __construct(IVideoRepository $videoRepo)
     {
         $this->videoRepo = $videoRepo;
     }
 
+    /**
+     * search
+     *
+     * @param  int $countryTopicId
+     * @param  mixed $search
+     * @return void
+     */
     public function search(int $countryTopicId, string $search)
     {
-        $response = collect(
-            VideoResource::collection(
-                $this->videoRepo->search($countryTopicId, $search)
-            )
-        )->toArray();
-        return $response;
+        $data = $this->videoRepo->search($countryTopicId, $search);
+
+        return $this->formatResponse('videoSearch', $data);
     }
 
+    /**
+     * index
+     *
+     * @param  mixed $countryTopicId
+     * @return void
+     */
     public function index(int $countryTopicId)
     {
-        $response = collect(
-            VideoResource::collection(
-                $this->videoRepo->getVideos($countryTopicId)
-            )
-        )->toArray();
+        $data = $this->videoRepo->getVideos($countryTopicId);
 
-        return $response;
+        return $this->formatResponse('video', $data);
+    }
+
+    /**
+     * formatResponse
+     *
+     * @param  mixed $type
+     * @param  mixed $data
+     * @return array
+     */
+    public function formatResponse(string $type, mixed $data): array
+    {
+        switch ($type) {
+            case 'video':
+                return collect(
+                    VideoResource::collection(
+                        $data
+                    )
+                )->toArray();
+            case 'videoSearch':
+                return collect(
+                    VideoSearchResource::collection(
+                        $data
+                    )
+                )->toArray();
+            default:
+                return [];
+        }
     }
 }
