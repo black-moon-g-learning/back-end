3a21decfa2280924730b907e677700e8752b78db
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-02-06 14:20:54 +0700

    add owner id column

--- database/migrations/2023_02_06_041343_create_contributes_table.php
+++ database/migrations/2023_02_06_041343_create_contributes_table.php
@@ -21,10 +21,12 @@ return new class extends Migration
             $table->text('description');
             $table->text('image');
             $table->text('video');
-            $table->integer('country_id')->unsigned();
+            $table->integer('country_id')->unsigned()->nullable();
+            $table->integer('owner_id')->unsigned()->nullable();
 
             $this->createForeignKey($table, 'country_id', 'countries');
-            
+            $this->createForeignKey($table, 'owner_id', 'users');
+
             $table->timestamps();
         });
     }
