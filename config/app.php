73894849b9457b38535aa0cea1c4aad44a49e98c
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-02-13 20:04:55 +0700

    add aliases for view

--- config/app.php
+++ config/app.php
@@ -211,6 +211,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Information' => App\Constants\Information::class
     ])->toArray(),
 
 ];
