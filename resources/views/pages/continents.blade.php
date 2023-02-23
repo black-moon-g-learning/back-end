8c140945644aad1eeb73e4664eff911209229c19
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-02-23 23:55:39 +0700

    update image size in continents

--- resources/views/pages/continents.blade.php
+++ resources/views/pages/continents.blade.php
@@ -41,8 +41,8 @@
                                         <td>
                                             <div class="d-flex px-2 py-1">
                                                 <div>
-                                                    <img src="{{ getS3Url($continent->image) }}"
-                                                        class="avatar avatar-sm me-3" alt="user1">
+                                                    <img src="{{ getS3Url($continent->image) }}" style="width:200px"
+                                                        alt="user1">
                                                 </div>
                                                 <div class="d-flex flex-column justify-content-center">
                                                     <h6 class="mb-0 text-sm">{{ $continent->name }}
