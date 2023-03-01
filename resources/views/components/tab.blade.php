416669ad03aa021ad252c7c5e759d9d324de0a6c
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-03-01 09:57:51 +0700

    fix bug get id

--- resources/views/components/tab.blade.php
+++ resources/views/components/tab.blade.php
@@ -1,9 +1,11 @@
 <li class="nav-item">
-    <a class="nav-link {{ (request()->fullUrl() == $tab['route'])|| (str_contains(request()->fullUrl(),$tab['route'].'?page')) ? 'active' : '' }}" href="{{ $tab['route'] }}">
+    <a id="category-{{ $id }}"
+        class="nav-link {{ request()->fullUrl() == $route || str_contains(request()->fullUrl(), $route . '?page') ? 'active' : '' }}"
+        href="{{ $route }}">
         <div
             class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
             <i class="ni ni-tv-2 text-primary text-sm opacity-10"></i>
         </div>
-        <span class="nav-link-text ms-1">{{ $tab['name'] }}</span>
+        <span class="nav-link-text ms-1">{{ $name }}</span>
     </a>
 </li>
