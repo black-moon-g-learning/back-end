deb1e5fb5b04eb623c34c263c9781841e9be3eb8
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-03-04 08:29:28 +0700

    add middleware expired trial day

--- app/Constants/Common.php
+++ app/Constants/Common.php
@@ -7,4 +7,8 @@ final class Common
     const S3_ROOT = "https://g-learning-v1.s3.ap-southeast-1.amazonaws.com/";
 
     const YOUTUBE_EMBED = "https://www.youtube.com/embed/";
+
+    const FREE_DAY_TRIAL = 7;
+
+    const DEFAULT_EXPIRED_TRIAL = -1;
 }
