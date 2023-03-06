47ee2ce9776296332b140aa43c66afaeebe5b5af
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-03-06 22:50:00 +0700

    store history

---
+++ app/Models/Watched.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Watched extends Model
+{
+    use HasFactory;
+
+    protected $table = 'watched';
+
+    protected $fillable = [
+        'user_id',
+        'video_id',
+        'time'
+    ];
+}
