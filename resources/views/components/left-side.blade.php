f11cb00a059962a8f7e31b2e60c21e7bd22fe49a
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-03-06 15:55:04 +0700

    remove image unneccesery

--- resources/views/components/left-side.blade.php
+++ resources/views/components/left-side.blade.php
@@ -46,7 +46,7 @@
             aria-hidden="true" id="iconSidenav"></i>
         <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html "
             target="_blank">
-            <img src="{{ asset('img/g-learning.png') }}" class="navbar-brand-img h-100" alt="main_logo">
+            <img src="{{ asset('favicon.png') }}" class="navbar-brand-img h-100" alt="main_logo">
             <span class="ms-1 font-weight-bold">G-learning Dashboard</span>
         </a>
     </div>
