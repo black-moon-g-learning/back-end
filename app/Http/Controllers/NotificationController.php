a72851638afa87e885b4b802b61783063bf04379
Tran Van Hieu <hieu.tran23@student.passerellesnumeriques.org>
2023-03-09 23:08:12 +0700

    push notification

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Constants\Common;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class NotificationController extends Controller
+{
+
+    public function index()
+    {
+        return view('pages.home');
+    }
+    public function saveToken(Request $request)
+    {
+
+        $user =  auth()->user();
+        $user->device_token = $request->token;
+        $user->save();
+        return response()->json(['token saved successfully.']);
+    }
+
+    /**
+     * Write code on Method
+     *
+     * @return response()
+     */
+    public function sendNotification(Request $request)
+    {
+        $firebaseToken = User::whereNotNull('device_token')->pluck('device_token')->all();
+
+
+        $data = [
+            "registration_ids" => $firebaseToken,
+            "notification" => [
+                "title" => $request->title,
+                "body" => $request->body,
+            ]
+        ];
+        $dataString = json_encode($data);
+
+        $headers = [
+            'Authorization: key=' . Common::SERVER_API_KEY,
+            'Content-Type: application/json',
+        ];
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, 'https://fcm.googleapis.com/fcm/send');
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $dataString);
+
+        $response = curl_exec($ch);
+
+        dd($response);
+    }
+}
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.master')
+
+@section('content')
+    <div class="container">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <center>
+                    <button id="btn-nft-enable" onclick="initFirebaseMessagingRegistration()"
+                        class="btn btn-danger btn-xs btn-flat">Allow for Notification</button>
+                </center>
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
+
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <form action="{{ route('send.notification') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label>Title</label>
+                                <input type="text" class="form-control" name="title">
+                            </div>
+                            <div class="form-group">
+                                <label>Body</label>
+                                <textarea class="form-control" name="body"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Send Notification</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.4/jquery.min.js"
+        integrity="sha512-pumBsjNRGGqkPzKHndZMaAG+bir374sORyzM3uulLV14lN5LyykqNk8eEeUlUkB3U0M4FApyaHraT65ihJhDpQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.23.0/firebase.js"></script>
+    <script>
+        var firebaseConfig = {
+            apiKey: "AIzaSyB6-BTU2CQ9306VecGTYXsar7s4jAK1guk",
+            authDomain: "g-learning-a4e2f.firebaseapp.com",
+            projectId: "g-learning-a4e2f",
+            storageBucket: "g-learning-a4e2f.appspot.com",
+            messagingSenderId: "1082282985717",
+            appId: "1:1082282985717:web:dc8b7e034eb5ee9f495dbc",
+            measurementId: "G-KTDRKEHVV9"
+        };
+
+        firebase.initializeApp(firebaseConfig);
+        const messaging = firebase.messaging();
+
+        function initFirebaseMessagingRegistration() {
+            messaging
+                .requestPermission()
+                .then(function() {
+                    return messaging.getToken()
+                })
+                .then(function(token) {
+                    console.log(token);
+
+                    $.ajaxSetup({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        }
+                    });
+
+                    $.ajax({
+                        url: '{{ route('save-token') }}',
+                        type: 'POST',
+                        data: {
+                            token: token
+                        },
+                        dataType: 'JSON',
+                        success: function(response) {
+                            alert('Token saved successfully.');
+                        },
+                        error: function(err) {
+                            console.log('User Chat Token Error' + err);
+                        },
+                    });
+
+                }).catch(function(err) {
+                    console.log('User Chat Token Error' + err);
+                });
+        }
+
+        messaging.onMessage(function(payload) {
+            const noteTitle = payload.notification.title;
+            const noteOptions = {
+                body: payload.notification.body,
+                icon: payload.notification.icon,
+            };
+            new Notification(noteTitle, noteOptions);
+        });
+    </script>
+@endsection
